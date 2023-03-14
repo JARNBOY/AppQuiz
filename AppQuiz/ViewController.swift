16edb4c51a1d347bef5cc3684ae866db764b71c3
Papon <papon.s@lawyerplusgroup.com>
2023-03-14 16:17:19 +0700

    Initial Commit

---
+++ AppQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppQuiz
+//
+//  Created by Papon Supamongkonchai on 14/3/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
